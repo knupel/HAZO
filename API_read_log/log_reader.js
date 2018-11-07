2bf43347383309021c9388a2c19aa9bf2196a5ff
Stan le Punk <stanislasiii@gmail.com>
2018-11-07 18:35:48 +0100

    improve display system to draw user

--- API_read_log/log_reader.js
+++ API_read_log/log_reader.js
@@ -21,7 +21,7 @@ function display_log(font,size) {
   // text('dist:'+ radius_log, 100,100);
   // text('cx:'+ colour_log_x + ' cy:'+ colour_log_y +' cz:'+ colour_log_z, 100,100);
   var s = split(String(result[target_log]),'|');
-  var pos = position(.01,100);
+  var pos = position(.01,windowHeight/2);
   for(var i = 0 ; i < s.length ; i++) {
   	var pos_x =  mouseX + pos.x;
   	var pos_y =  mouseY + (i*size) + pos.y;
@@ -45,9 +45,23 @@ function colour_inc(speed_x,speed_y,speed_z) {
 
 
 var radius_log = 0;
+var growth_is = false;
 function position(speed,dist_max) {
-	radius_log += (speed*5);
-	if(radius_log > dist_max) radius_log = 0 ;
+	var inc_radius = speed*5;
+	if(!growth_is) {
+		radius_log += inc_radius;
+	} else {
+		radius_log += (-1 *inc_radius);
+	}
+
+	if(radius_log > dist_max) {
+		growth_is = true;
+	} 
+	if(radius_log <0) {
+		growth_is = false;
+	}
+
+
 	var angle = frameCount *speed;
 	var x = cos(angle) *radius_log;
 	var y = sin(angle) *radius_log;
